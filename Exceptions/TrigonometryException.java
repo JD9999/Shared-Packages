421e64cb03625c387a2adf1064c783817b9792f0
JD9999 <jamiemdouglass@gmail.com>
2017-06-04 12:14:12 +1000

    Updated to V1.2.1
    Uploaded all other exceptions

---
+++ Exceptions/TrigonometryException.java
@@ -0,0 +1,24 @@
+package Exceptions;
+
+/**
+ * The exception related to trigonometry errors
+ */
+public class TrigonometryException extends Exception {
+
+	String message;
+	
+	/**
+	 * The constructor
+	 * @param string the message to come up
+	 */
+	public TrigonometryException(String string) {
+		message = string;
+	}
+
+	private static final long serialVersionUID = 4916560792104825371L;
+
+	public String getMessage(){
+		return message;
+	}
+	
+}
