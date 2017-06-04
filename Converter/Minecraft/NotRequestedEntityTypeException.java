ec02f114eea62f2a3132dfd86fc33061bff155b5
JD9999 <jamiemdouglass@gmail.com>
2017-06-04 12:08:55 +1000

    Updated to V1.2.1
    Added Entity converter

---
+++ Converter/Minecraft/NotRequestedEntityTypeException.java
@@ -0,0 +1,39 @@
+package Converter.Minecraft;
+
+import PluginReference.MC_Entity;
+
+public class NotRequestedEntityTypeException extends Exception{
+
+	/**
+	 * The reason for the exception
+	 */
+	String reason;
+	private static final long serialVersionUID = 513L;
+	
+	/**
+	 * The entity constructor
+	 * Creates a NotRequestedEntityTypeException object, identifying the entity in the reason
+	 * @param ent The entity that is not the requested type
+	 */
+	public NotRequestedEntityTypeException(MC_Entity ent) {
+		this("Entity " + ent.getName() + " is not the requested type!");
+	}
+
+	/**
+	 * The reason constructor
+	 * Creates a NotRequestedEntityTypeException object, from the reason provided.
+	 * @param string The reason for the exception
+	 */
+	public NotRequestedEntityTypeException(String string) {
+		reason = string;
+	}
+
+	/**
+	 * Gets the reason for the exception
+	 * @return the reason for the exception
+	 */
+	public String getMessage(){
+		return reason;
+	}
+
+}
